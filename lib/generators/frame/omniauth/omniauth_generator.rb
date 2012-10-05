ee8a165fb3ad358bbb0f1802f283befb9152f3eb
Nik <nik.cimino@gmail.com>
2012-10-04 21:47:37 -0600

    sasified the omiauth genome

--- lib/generators/frame/omniauth/omniauth_generator.rb
+++ lib/generators/frame/omniauth/omniauth_generator.rb
@@ -42,20 +42,20 @@ module Frame
 
     # Leave the routes?
     def generate_authentication_controller
-      generate("controller Authentications index create destroy")
-      template 'authentications_controller.rb', 'app/controllers/authentications_controller.rb'
-      remove_file 'app/views/authentications/index.html.erb'
-      remove_file 'app/views/authentications/create.html.erb'
-      remove_file 'app/views/authentications/destroy.html.erb'
+      generate('controller Authentications index create destroy')
+      template('app/controllers/authentications_controller.rb')
+      remove_file('app/views/authentications/index.html.erb')
+      remove_file('app/views/authentications/create.html.erb')
+      remove_file('app/views/authentications/destroy.html.erb')
       # Need this index?
-      template 'index.html.erb', 'app/views/authentications/index.html.erb'
+      template('app/views/authentications/index.html.erb')
     end
 
     def generate_registration_controller
-      generate("controller Registrations edit new")
-      template 'registrations/edit.html.erb', 'app/views/registrations/edit.html.erb'
-      template 'registrations/new.html.erb', 'app/views/registrations/new.html.erb'
-      template 'registrations_controller.rb', 'app/controllers/registrations_controller.rb'
+      generate('controller Registrations edit new')
+      template('app/views/registrations/edit.html.erb')
+      template('app/views/registrations/new.html.erb')
+      template('app/controllers/registrations_controller.rb')
     end
 
     def add_routes
@@ -70,13 +70,24 @@ module Frame
     end
 
     def update_user_model
-      template('user.rb', 'app/models/user.rb')
+      template('app/models/user.rb')
+    end
+
+    def copy_images
+      template('app/assets/images/facebook_32.png')
+      template('app/assets/images/github_32.png')
+      template('app/assets/images/google_32.png')
+      template('app/assets/images/twitter_32.png')
+      template('app/assets/images/facebook_64.png')
+      template('app/assets/images/github_64.png')
+      template('app/assets/images/google_64.png')
+      template('app/assets/images/twitter_64.png')
     end
 
     def add_sign_in_to_devise
-      template('new.html.erb', 'app/views/devise/sessions/new.html.erb')
-      template('_open_auth.html.erb', 'app/views/shared/_open_auth.html.erb')
-      template('_login_form.html.erb', 'app/views/shared/_login_form.html.erb')
+      template('app/views/devise/sessions/new.html.erb')
+      template('app/views/shared/_open_auth.html.erb')
+      template('app/views/shared/_login_form.html.erb')
     end
 
     def update_db
